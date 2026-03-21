2e63299cffee43d37386e645372d89dd8779f55e
slyuter <slyuter@ozon.ru>
2026-03-21 18:59:47 +0700

    feat(storage): add isPanelOpen state and persistence

--- storage.js
+++ storage.js
@@ -1,21 +1,26 @@
 const rfStorage = {
     blacklistedCountries: [],
     targetVisibleCount: 36, // По умолчанию на странице rezka 36 карточек
+    isPanelOpen: true,
     
     save: function() {
         chrome.storage.local.set({ 
             blacklistedCountries: this.blacklistedCountries,
-            targetVisibleCount: this.targetVisibleCount
+            targetVisibleCount: this.targetVisibleCount,
+            isPanelOpen: this.isPanelOpen
         });
     },
     
     load: function(callback) {
-        chrome.storage.local.get(['blacklistedCountries', 'targetVisibleCount'], (result) => {
+        chrome.storage.local.get(['blacklistedCountries', 'targetVisibleCount', 'isPanelOpen'], (result) => {
             if (result.blacklistedCountries) {
                 this.blacklistedCountries = result.blacklistedCountries;
             }
             if (result.targetVisibleCount) {
                 this.targetVisibleCount = result.targetVisibleCount;
+            }
+            if (result.isPanelOpen !== undefined) {
+                this.isPanelOpen = result.isPanelOpen;
             }
             callback();
         });
@@ -34,6 +39,11 @@ const rfStorage = {
     
     setTargetCount: function(count) {
         this.targetVisibleCount = count;
+        this.save();
+    },
+
+    setPanelOpen: function(isOpen) {
+        this.isPanelOpen = isOpen;
         this.save();
     }
 };
